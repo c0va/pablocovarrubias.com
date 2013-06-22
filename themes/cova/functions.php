85f45fe3b1610efef1f4f6d6d87da016fc85d01d
cova <cova@losmaquiladores.com>
2013-06-22 05:49:22 -0500

    sólo falta el footer

--- themes/cova/functions.php
+++ themes/cova/functions.php
@@ -5,4 +5,6 @@
 		add_theme_support( 'post-thumbnails' );
 		add_image_size('blog', 1024, 409, true);
 
+		//NAVEGACIÓN DEL BLOG
+
 ?>
--- themes/cova/header.php
+++ themes/cova/header.php
@@ -7,9 +7,19 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('stylesheet_url'); ?>">
 	</head>
 	<body>
+		<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=172546929510591";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 		<header>
+			<a href="<?php echo bloginfo('url'); ?>">
 				<div id="logo">
 					<img src="<?php echo get_bloginfo('template_url'); ?>/images/logo.png">
 				</div>
+			</a>
 		</header>
 		<div id="main">
