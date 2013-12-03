523672691999e08e5d93a911dca809cdb84deceb
Pablo Covarrubias <cova@Pablos-MacBook-Pro.local>
2013-12-02 20:26:03 -0600

    header changes

--- themes/cova/header.php
+++ themes/cova/header.php
@@ -2,6 +2,7 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link rel="shortcut icon" href="<?php bloginfo(template_url); ?>"/images/favicon.ico>
 		<meta name="viewport" content="width=device-width; initial-scale=1.0;">
 		<title><?php bloginfo('name'); ?> <?php wp_title(); ?></title>
 		<link rel="stylesheet" type="text/css" href="<?php echo get_bloginfo('stylesheet_url'); ?>">
